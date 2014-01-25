474b8d7a5a023ebdc69402367cd6a3eac32e716d
Leonardo Proietti <leonardo.proietti@gmail.com>
2014-01-25 04:50:45 +0100

    some fix

--- Validator/Constraints/UniqueEntityValidator.php
+++ Validator/Constraints/UniqueEntityValidator.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
-use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntityValidator as BaseValidator;
 
 class UniqueEntityValidator extends BaseValidator
