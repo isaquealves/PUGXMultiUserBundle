0763fede599a48b786442991be7b4bbc7e665198
Sydney-o9 <anthony.rey21@gmail.com>
2014-10-10 12:42:06 +1100

    removed lines

--- Controller/RegistrationManager.php
+++ Controller/RegistrationManager.php
@@ -12,35 +12,35 @@ class RegistrationManager
 {
     /**
      *
-     * @var \PUGX\MultiUserBundle\Model\UserDiscriminator 
+     * @var \PUGX\MultiUserBundle\Model\UserDiscriminator
      */
     protected $userDiscriminator;
-    
+
     /**
      *
-     * @var \Symfony\Component\DependencyInjection\ContainerInterface 
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
      */
     protected $container;
-    
+
     /**
      *
-     * @var \FOS\UserBundle\Controller\RegistrationController 
+     * @var \FOS\UserBundle\Controller\RegistrationController
      */
     protected $controller;
-    
+
     /**
      *
      * @var \PUGX\MultiUserBundle\Form\FormFactory
      */
     protected $formFactory;
-        
+
     /**
-     * 
+     *
      * @param \PUGX\MultiUserBundle\Model\UserDiscriminator $userDiscriminator
      * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
      */
     public function __construct(UserDiscriminator $userDiscriminator,
-                                ContainerInterface $container, 
+                                ContainerInterface $container,
                                 RegistrationController $controller,
                                 FormFactory $formFactory)
     {
@@ -49,29 +49,29 @@ class RegistrationManager
         $this->controller = $controller;
         $this->formFactory = $formFactory;
     }
-    
+
     /**
-     * 
+     *
      * @param string $class
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function register($class)
     {
         $this->userDiscriminator->setClass($class);
-        
+
         $this->controller->setContainer($this->container);
-        $result = $this->controller->registerAction($this->container->get('request'));        
+        $result = $this->controller->registerAction($this->container->get('request'));
         if ($result instanceof RedirectResponse) {
             return $result;
         }
-        
+
         $template = $this->userDiscriminator->getTemplate('registration');
         if (is_null($template)) {
             $engine = $this->container->getParameter('fos_user.template.engine');
             $template = 'FOSUserBundle:Registration:register.html.'.$engine;
         }
-        
-        $form = $this->formFactory->createForm();      
+
+        $form = $this->formFactory->createForm();
         return $this->container->get('templating')->renderResponse($template, array(
             'form' => $form->createView(),
         ));
